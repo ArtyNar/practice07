c2a4a99d977913e182cfa722f1c4aa521c1457ff
Artem <arty.nar00@gmail.com>
2020-03-26 16:10:17 -0600

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -3,5 +3,57 @@ require.context('../', true, /\.(html|json|txt|dat)$/i)
 require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
+import 'bootstrap'
 // JavaScript
 //TODO
+
+const displayRandomBox = function()
+{
+    let rand = Math.random()
+    if(rand < .5)
+    {
+        document.write('<div class="red box"></div>')
+    }
+    else
+    {
+        document.write('<div class="blue box"></div>')
+    }
+}
+
+function displayNBoxesUsingFor(n)
+{
+    for(let i = 0; i < n; i++)
+    {
+    displayRandomBox()
+    }
+}
+
+function displayNBoxesUsingWhile(n)
+{
+    let j = 0
+    while(j < n)
+    {
+        displayRandomBox()
+        j++
+    }
+}
+
+function displayNBoxesUsingDoWgile(n)
+{
+    let k = 0
+    do
+    {
+        displayRandomBox()
+        k++
+    }
+    while(k < n)
+}
+
+displayNBoxesUsingFor(15)
+displayNBoxesUsingWhile(12)
+displayNBoxesUsingDoWgile(14)
+
+const square = function(x)
+{
+    return x*x
+}
