59e12f625d117a3fdfdcab87c96a3f80a323450a
Artem <arty.nar00@gmail.com>
2020-03-26 16:32:53 -0600

    The Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -7,53 +7,8 @@ import 'bootstrap'
 // JavaScript
 //TODO
 
-const displayRandomBox = function()
+document.getElementById("submit").onclick = function displayMessage()
 {
-    let rand = Math.random()
-    if(rand < .5)
-    {
-        document.write('<div class="red box"></div>')
-    }
-    else
-    {
-        document.write('<div class="blue box"></div>')
-    }
-}
-
-function displayNBoxesUsingFor(n)
-{
-    for(let i = 0; i < n; i++)
-    {
-    displayRandomBox()
-    }
-}
-
-function displayNBoxesUsingWhile(n)
-{
-    let j = 0
-    while(j < n)
-    {
-        displayRandomBox()
-        j++
-    }
-}
-
-function displayNBoxesUsingDoWgile(n)
-{
-    let k = 0
-    do
-    {
-        displayRandomBox()
-        k++
-    }
-    while(k < n)
-}
-
-displayNBoxesUsingFor(15)
-displayNBoxesUsingWhile(12)
-displayNBoxesUsingDoWgile(14)
-
-const square = function(x)
-{
-    return x*x
+    let name = document.getElementById("name").value
+    alert("Welcome " + name + "!");
 }
